43baadd0c6e056f64081a68652d4bad72ad8da4a
Nikolay <krot19831983@mail.ru>
2023-02-16 13:11:48 +0300

    deleted:    Lecture02/ansible/inventory 	deleted:    Lecture02/ansible/roles/lb/files/hosts 	new file:   Lecture03/ansible/ansible.cfg 	new file:   Lecture03/ansible/db.yml 	new file:   Lecture03/ansible/roles/db/files/docker-compose.yml 	new file:   Lecture03/ansible/roles/db/tasks/docker.yml 	new file:   Lecture03/ansible/roles/db/tasks/install.yml 	new file:   Lecture03/ansible/roles/db/tasks/main.yml 	new file:   Lecture03/ansible/roles/db/vars/main.yml 	new file:   Lecture03/terraform/.terraform.lock.hcl 	new file:   Lecture03/terraform/db.tf 	renamed:    Lecture02/ansible/hosts -> Lecture03/terraform/hosts.tmpl 	new file:   Lecture03/terraform/inventory.tmpl 	new file:   Lecture03/terraform/main.tf 	new file:   Lecture03/terraform/modules/gfs2/main.tf 	new file:   Lecture03/terraform/modules/gfs2/variables.tf 	new file:   Lecture03/terraform/modules/iscsi-client/main.tf 	new file:   Lecture03/terraform/modules/iscsi-client/variables.tf 	new file:   Lecture03/terraform/modules/vpc/main.tf 	new file:   Lecture03/terraform/outputs.tf 	new file:   Lecture03/terraform/terraform.tfvars.example 	new file:   Lecture03/terraform/variables.tf 	new file:   Lecture03/terraform/vpc.tf

---
+++ Lecture03/terraform/variables.tf
@@ -0,0 +1,23 @@
+variable token {
+  type = string
+}
+
+variable cloud_id {
+  type = string
+}
+
+variable folder_id {
+  type = string
+}
+
+variable zone {
+  type = string
+}
+
+variable ssh_pub_key_file {
+  type = string
+}
+
+variable ssh_user {
+  type = string
+}
